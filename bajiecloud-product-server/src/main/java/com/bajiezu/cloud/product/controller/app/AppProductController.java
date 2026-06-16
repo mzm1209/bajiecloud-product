731e01a9bd75c42e930f07e3bd6f7db6f8bf65d8
xzhan_super <xzhan_super@163.com>
2026-06-16 11:33:51 +0800

    允许未登录客户在 App 端查看商品列表和详情信息

--- bajiecloud-product-server/src/main/java/com/bajiezu/cloud/product/controller/app/AppProductController.java
+++ bajiecloud-product-server/src/main/java/com/bajiezu/cloud/product/controller/app/AppProductController.java
@@ -8,6 +8,7 @@ import com.bajiezu.cloud.product.service.app.AppProductQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import jakarta.annotation.security.PermitAll;
 import jakarta.validation.Valid;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -23,11 +24,15 @@ public class AppProductController {
     private AppProductQueryService appProductQueryService;
 
     @PostMapping("/spu/page") @Operation(summary = "App端SPU分页")
+    @PermitAll
     public CommonResult<PageResult<AppProductSpuPageRespVO>> spuPage(@Valid @RequestBody AppProductSpuPageReqVO reqVO){return CommonResult.success(appProductQueryService.spuPage(reqVO));}
     @PostMapping("/spu/detail") @Operation(summary = "App端SPU详情")
+    @PermitAll
     public CommonResult<AppProductSpuDetailRespVO> spuDetail(@Valid @RequestBody AppProductSpuDetailReqVO reqVO){return CommonResult.success(appProductQueryService.spuDetail(reqVO));}
     @PostMapping("/sku/list") @Operation(summary = "App端SKU列表")
+    @PermitAll
     public CommonResult<List<AppProductSkuRespVO>> skuList(@Valid @RequestBody AppProductSkuListReqVO reqVO){return CommonResult.success(appProductQueryService.skuList(reqVO));}
     @PostMapping("/sku/detail") @Operation(summary = "App端SKU详情")
+    @PermitAll
     public CommonResult<AppProductSkuRespVO> skuDetail(@Valid @RequestBody AppProductSkuDetailReqVO reqVO){return CommonResult.success(appProductQueryService.skuDetail(reqVO));}
 }
